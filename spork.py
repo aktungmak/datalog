484c63de2a99d39e95ca879a7e10ea31d7043ea6
Joshua Green <aktungmak@gmail.com>
2023-05-19 23:07:00 +0200

    wip insert logic

--- spork.py
+++ spork.py
@@ -1,6 +1,8 @@
+from functools import reduce
 from typing import Dict, List, Tuple, Optional
 
-import pandas as pd
+import delta
+import pyspark.sql
 from pyspark.sql.types import StructType, StructField, LongType, StringType, TimestampType
 
 Subject = str
@@ -10,26 +12,28 @@ MatchPattern = Tuple[Subject, Predicate, Object]
 Binding = Tuple[str, str]
 SelectResult = list
 
-quad_table_name = "_quads"
+fact_table_name = "_facts"
 kv_string_table_name = "_1"
-quad_table_schema: StructType(
+fact_table_schema = StructType(
     [StructField("subject", LongType(), False),
-     StructField("subject_type", LongType(), False),
      StructField("predicate", LongType(), False),
-     StructField("predicate_type", LongType(), False),
      StructField("object", LongType(), False),
-     StructField("object_type", LongType(), False),
      StructField("graph", LongType(), True),
-     StructField("timestamp", TimestampType(), False)])
+     StructField("timestamp", TimestampType(), False),
+     StructField("subject_type", LongType(), False),
+     StructField("predicate_type", LongType(), False),
+     StructField("object_type", LongType(), False)])
 
 
 def kv_schema(value_type) -> StructType:
-    return StructType([StructField("key", LongType, False), StructField("value", value_type, False)])
+    return StructType([StructField("key", LongType(), False), StructField("value", value_type(), False)])
 
 
 class KG:
 
-    def __init__(self, spark_session, metastore_prefix: Optional[str] = None, path: Optional[str] = None,
+    def __init__(self, spark_session,
+                 metastore_prefix: Optional[str] = None,
+                 path: Optional[str] = None,
                  prefixes: Dict[str, str] = {},
                  property_tables: Dict[str, str] = {}):
         self.ss = spark_session
@@ -37,6 +41,7 @@ class KG:
         self.path = path
         self.prefixes = prefixes
         self.property_tables = property_tables
+        self.facts = None
 
     def load(self):
         if self.metastore_prefix is not None:
@@ -55,67 +60,75 @@ class KG:
             raise ValueError('One of metastore_prefix or path must be provided')
 
     def _load_from_metastore(self):
-        if self.ss.catalog.tableExists(self.metastore_prefix+quad_table_name):
-            self.quad = self.ss.getTable(self.metastore_prefix+quad_table_name)
+        if self.ss.catalog.tableExists(self.metastore_prefix + fact_table_name):
+            self.facts = self.ss.table(self.metastore_prefix + fact_table_name)
         else:
-            self.quad = self.ss.createTable(self.metastore_prefix+quad_table_name, source='delta', schema=quad_table_schema)
-        if self.ss.catalog.tableExists(self.metastore_prefix+kv_string_table_name):
-            self.kv_string = self.ss.getTable(self.metastore_prefix+kv_string_table_name)
+            self.facts = self.ss.createDataFrame([], spork.fact_table_schema)
+        if self.ss.catalog.tableExists(self.metastore_prefix + kv_string_table_name):
+            self.kv_string = self.ss.table(self.metastore_prefix + kv_string_table_name)
         else:
-            self.kv_string = self.ss.createTable(self.metastore_prefix+kv_string_table_name, source='delta', schema=kv_schema(StringType))
+            self.kv_string = self.ss.createDataFrame([], schema=kv_schema(StringType))
 
-
-    def select(self, projection: List[str], matches: List[MatchPattern], optional_clauses: List[MatchPattern] = [],
+    def select(self, projection: List[str], matches: List[MatchPattern],
+               optional_clauses: List[MatchPattern] = [],
                bindings: List[Binding] = [],
                distinct: bool = False, limit: int = 1000) -> SelectResult:
 
-        for subject, predicate, object in matches:
-            if is_var(subject):
-                pass
-            else:
+        for match in matches:
+            mdf = self.facts
+            for i, elem in enumerate(match):
+                if is_var(elem):
+                    mdf = mdf.withColumnRenamed(mdf.columns[i], elem)
+                else:
+                    # join to kv table based on type
+                    kv_table = self.type_to_table(elem)
+                    mdf = mdf.join(kv_table, (kv_table['value'] == elem) & (mdf[i] == kv_table['key']))
 
         df.select(projection)
 
+    def insert(self, facts: list):
+        octs = []
+        for f in facts:
+            if len(f) == 3:
+                octs.append(f + [None, 0] + [pytype_to_int(e) for e in f])
+            if len(f) == 4:
+                octs.append(f + [0] + [pytype_to_int(e) for e in f[:3]])
+            if len(f) == 5:
+                octs.append(f + [pytype_to_int(e) for e in f[:3]])
+        new_df = self.ss.createDataFrame(octs, fact_table_schema)
+        # extract the different types by doing a flatmap and filter over the values
+        # merge into the KVs
+        # semijoin to get the IDs
+        # insert into the fact table
+        self.facts = self.facts.unionAll(new_df).distinct()
 
-def manual():
-    matches = [("?id", ":name", "?age")]
+
+def pytype_to_int(obj):
+    if obj is None:
+        return -1
+    if type(obj) is int:
+        return 0
+    elif type(obj) is str:
+        return 1
+    else:
+        raise ValueError(f"unknown object type {type(obj)}")
 
 
 def is_var(name: str) -> bool:
     return type(name) is str and name.startswith('?')
 
 
-def query(
-        triples: pd.DataFrame, projection: list, clauses: list, distinct: bool = False
-) -> pd.DataFrame:
-    for clause in clauses:
-        triples = apply_clause(clause, triples)
-    # project out relevant columns
-    triples = triples.filter(items=projection)
-    # extract distince values
-    if distinct:
-        triples.drop_duplicates(inplace=True)
-    return triples
+def multiple_join(dfs: list[pyspark.sql.DataFrame]) -> pyspark.sql.DataFrame:
+    def join(left, right):
+        on = list(set(left.columns) & set(right.columns))
+        return left.join(right, on=on, how='inner')
+
+    return reduce(join, dfs)
 
 
-def apply_clause(clause, triples):
-    clause_type = clause_type(clause)
-    if clause_type == "allvar":
-        return allvar(parsed_clause, triples)
-    elif clause_type == "mixed":
-        return mixed(parsed_clause, triples)
-    elif clause_type == "prop_path":
-        return prop_path(parsed_clause, triples)
-    elif clause_type == "multi_object":
-        return multi_object(parsed_clause, triples)
-    else:
-        assert False
+def create_spark_session():
+    builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
 
-
-def clause_type(clause: list) -> str:
-    if all(map(lambda s: s.startswith("?"), clause)):
-        return "allvar"
-    elif all(map(lambda x: isinstance(int, x) or isinstance(str, x), clause)):
-        return "mixed"
-    elif isinstance(list, clause[1]):
-        return "prop_path"
+    return delta.configure_spark_with_delta_pip(builder).getOrCreate()
