0c94159ab5c50ad007c118bff2008553ee24e85e
Joshua Green <aktungmak@gmail.com>
2024-07-26 11:47:00 +0200

    rename datalog_ast to dast

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import parser
 from validator import NonRecursiveValidator
 from pandas_evaluator import PandasEvaluator
 
+
 def run(filename, validator=NonRecursiveValidator, evaluator=PandasEvaluator):
     program = parser.parse_file(filename)
     errors = list(validator(program).validate())
@@ -9,7 +10,9 @@ def run(filename, validator=NonRecursiveValidator, evaluator=PandasEvaluator):
         print(errors)
     return evaluator(program).run()
 
+
 if __name__ == '__main__':
     import sys
+
     res = run(sys.argv[1])
     print(res)
