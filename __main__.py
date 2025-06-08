a63b493e8a364027e62ba86e06b6681115d56526
Joshua Green <aktungmak@gmail.com>
2025-06-08 12:33:04 -0700

    start implementing Repl class

--- __main__.py
+++ __main__.py
@@ -1,27 +1,37 @@
 import parser
-from edb import EDB
-from validator import NonRecursiveValidator
+from dast import Fact, Rule, Program, Atom
+from edb import PandasEDB
 from pandas_evaluator import PandasEvaluator
+from validator import NonRecursiveValidator
 
 
-def repl(edb: EDB, filename: str, validator=NonRecursiveValidator, evaluator=PandasEvaluator):
-    while True:
-        try:
-            program = parser.parse_file(filename)
-            errors = list(validator(program).validate())
-            if len(errors) != 0:
-                print(errors)
-            ev = evaluator(program)
-            edb.insert_facts(program.facts)
-            query = parser.parse_string(input("> "))
-            res = ev.query(query)
-            edb.
-        except KeyboardInterrupt:
-            return
+class Repl:
+    def __init__(self, program_filename: str, validator=NonRecursiveValidator, evaluator=PandasEvaluator):
+        program = parser.parse_file(program_filename)
+        errors = list(validator(program).validate())
+        if len(errors) != 0:
+            print(errors)
+            exit(1)
+        self.evaluator = evaluator(program)
+
+    def loop(self):
+        while True:
+            try:
+                cmd = parser.parse_string(input("> "))
+                print(cmd)
+                if isinstance(cmd, Program):
+                    for clause in cmd.clauses:
+                        if isinstance(clause, Fact):
+                            self.evaluator.edb.insert_facts([clause])
+                        if isinstance(clause, Rule):
+                            self.evaluator.prog.rules.append(clause)
+                elif isinstance(cmd, Atom):
+                    print(self.evaluator.query(cmd))
+                else:
+                    print(f"unknown command type {type(clause)}")
+            except KeyboardInterrupt:
+                return
 
 
 if __name__ == '__main__':
-    import sys
-
-    res = run(sys.argv[1])
-    print(res)
+    Repl("sample_programs/good.dl").loop()
